021f1186a98f08d6f0ce5abe3b9c40350190b00c
Sunki Baek <sunki.baek@gmail.com>
2018-03-07 23:15:23 +0900

    Add typescript and related tools

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import { AppRegistry } from "react-native";
 
-AppRegistry.registerComponent('BlueMonkey', () => App);
+import App from "./src/App";
+
+AppRegistry.registerComponent("BlueMonkey", () => App);
